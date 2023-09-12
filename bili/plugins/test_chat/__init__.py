09e912e21f07a3eda3e51b6239109af2373c6bc0
Agnes Digital <Z735803792@163.com>
2023-09-12 18:05:46 +0800

    更改

--- bili/plugins/test_chat/__init__.py
+++ bili/plugins/test_chat/__init__.py
@@ -1,13 +1,23 @@
-from nonebot import on_command
+from nonebot import get_bot, get_driver, on_command, require
 
+require("nonebot_plugin_apscheduler")
 # from nonebot.adapters import Event
-from nonebot.adapters.bilibili.event import Danmu_msg
-from nonebot.matcher import Matcher
+# from nonebot.adapters.bilibili.bot import Bot
+# from nonebot.matcher import Matcher
+from nonebot_plugin_apscheduler import scheduler  # noqa: E402, I001
 
+
+driver = get_driver()
 test = on_command("测试")
 
 
-@test.handle()
-async def _(event: Danmu_msg, matcher: Matcher):
-    # event
-    await matcher.finish("收到")
+async def auto_cup():
+    await get_bot().send("你好")
+
+
+scheduler.add_job(auto_cup, "cron", hour=0, minute=1, id="auto_cup")
+
+
+@driver.on_bot_disconnect
+async def _():
+    scheduler.remove_job("auto_cup")
